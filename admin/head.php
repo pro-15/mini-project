99f1a92b0a3385b9884c2759f0845cc3c13548c5
user-014 <thakkudus04@gmail.com>
2023-06-08 20:13:58 +0530

    new update

---
+++ admin/head.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>OP Clinic</title>
+    <link href="assets/img/logo.png" rel="icon" />
+	<!-- BOOTSTRAP STYLES-->
+    <link href="assets/css/bootstrap.css" rel="stylesheet" />
+     <!-- FONTAWESOME STYLES-->
+    <link href="assets/css/font-awesome.css" rel="stylesheet" />
+        <!-- CUSTOM STYLES-->
+    <link href="assets/css/style.css" rel="stylesheet" />
+     <!-- GOOGLE FONTS-->
+   <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
+</head>
+<body>
+    <div id="wrapper">
+        <nav class="navbar navbar-default navbar-cls-top " role="navigation" style="margin-bottom: 0">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".sidebar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="index.html">Binary admin</a> 
+            </div>
+            <div style="color: white;
+                padding: 15px 50px 5px 50px;
+                float: right;
+                font-size: 16px;">
+                <a href="login.html" class="btn btn-danger square-btn-adjust">Logout</a>
+            </div>
+        </nav>   
+        <!-- /. NAV TOP  -->
+        <nav class="navbar-default navbar-side" role="navigation">
+            <div class="sidebar-collapse">
+                <ul class="nav" id="main-menu">
+				    <li class="text-center">
+                        <img src="assets/img/hosp_logo.png" class="user-image img-responsive"/>
+					</li>
+					<li>
+                        <a  href="in-emp.php"><i class="fa fa-user fa-3x"></i> REGISTER</a>
+                    </li>
+                    <li>
+                        <a  href="in-empimg.php"><i class="fa fa-user fa-3x"></i> IMAGE REGISTER</a>
+                    </li>
+                    <li>
+                        <a  href="in-cat.php"><i class="fa fa-user fa-3x"></i> CATEGORY ENTER</a>
+                    </li>
+                    <li>
+                        <a  href="in-item.php"><i class="fa fa-user fa-3x"></i>ITEM ENTER</a>
+                    </li>
+                    <li>
+                        <a  href="out-emp.php"><i class="fa fa-table fa-3x"></i>REGISTER VIEW</a>
+                    </li>
+                    <li>
+                        <a  href="out-empimg.php"><i class="fa fa-table fa-3x"></i> IMAGE VIEW</a>
+                    </li>
+                    <li>
+                        <a  href="out-cat.php"><i class="fa fa-table fa-3x"></i>CATEGORY VIEW</a>
+                    </li>
+                    <li>
+                        <a  href="out-item.php"><i class="fa fa-table fa-3x"></i>ITEM VIEW</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>  
+        <!-- /. NAV SIDE  -->
+    <div id="page-wrapper" >
+        <div id="page-inner">
