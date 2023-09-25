b3e8be0a15799ac5e30d34779252aefac7898ea9
user-014 <thakkudus04@gmail.com>
2023-09-25 20:08:37 +0530

    New leaf

--- user/login0.php
+++ user/login0.php
@@ -5,7 +5,7 @@
   <!-- Required meta tags -->
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <title>Purple Site</title>
+  <title>Health Center</title>
   <!-- plugins:css -->
   <link rel="stylesheet" href="assets/vendors/mdi/css/materialdesignicons.min.css">
   <link rel="stylesheet" href="assets/vendors/css/vendor.bundle.base.css">
